92de3df968811d3cee6c5cd1c5e3eb2f442fcfef
wxb2dyj <wxb2dyj@gmail.com>
2019-06-27 15:49:41 +0800

    delete a line in run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,3 @@ aa-remove-unknown
 docker stop webdemo-8452
 docker rm -f webdemo-8452
 docker run -d --name webdemo-8452  -p 8452:8452 lgj/webdemo:1.0
-
-
-docker ps -a | grep webdemo-8452
-#查看启动日志
-docker logs -f -t --tail 10  microblog-scheduler-web
